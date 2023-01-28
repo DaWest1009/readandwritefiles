57d7d1e6ea9150b81bacfa668b8eb506e16232a1
tasi <daunte_west1@baylor.edu>
2023-01-28 15:31:51 -0600

    almost done with hw

---
+++ customer_sales_report.py
@@ -0,0 +1,27 @@
+import csv
+
+original_file = 'sales.csv'
+new_file = 'salesreport.csv'
+
+infile = open(original_file, 'r',)
+outfile = open(new_file, 'w')
+
+reader = csv.reader(infile)
+writer = csv.writer(outfile, delimiter=',')
+
+fieldnames = next(reader)
+fieldnames[1] = 'Total'
+newfields = (fieldnames[0], fieldnames[1])
+writer.writerow(newfields)
+
+for row in reader:
+    id = row[0]
+    subtotal = float(row[3])
+    tax = float(row[4])
+    freight = float(row[5])
+    total = subtotal + tax + freight
+    newrow = [id, total]
+    writer.writerow(newrow)
+
+infile.close()
+outfile.close()
