708efedd0c8a45c4577d2cf66dbae8f5789303b1
tasi <daunte_west1@baylor.edu>
2023-01-30 15:58:08 -0600

    completed salesreport code

--- customer_sales_report.py
+++ customer_sales_report.py
@@ -14,14 +14,26 @@ fieldnames[1] = 'Total'
 newfields = (fieldnames[0], fieldnames[1])
 writer.writerow(newfields)
 
+count = 0
+last_cust = ''
+total = 0
+
 for row in reader:
     cust_id = row[0]
     subtotal = float(row[3])
     tax = float(row[4])
     freight = float(row[5])
-    total = subtotal + tax + freight
-    newrow = [cust_id, total]
-    writer.writerow(newrow)
+    if count == 0:
+        last_cust = cust_id
+    if cust_id  == last_cust:
+        total += subtotal + tax + freight
+    else:
+        writer.writerow((last_cust, format (total, '.2f')))
+        total = subtotal + tax + freight
+    last_cust = cust_id
+    count += 1
+writer.writerow((last_cust, format (total, '.2f')))
+
 
 infile.close()
 outfile.close()
